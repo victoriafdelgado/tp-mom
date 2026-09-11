4fbb006d485589979e0963f86fc89637e9348f7d
victoriafdelgado <vickyfdelgado@gmail.com>
2026-09-11 19:58:14 -0300

    implemento queue (faltan las excepciones)

--- python/src/common/middleware/middleware_rabbitmq.py
+++ python/src/common/middleware/middleware_rabbitmq.py
@@ -1,12 +1,44 @@
 import pika
 import random
 import string
-from .middleware import MessageMiddlewareQueue, MessageMiddlewareExchange
+from .middleware import MessageMiddlewareQueue, MessageMiddlewareExchange, MessageMiddlewareCloseError, MessageMiddlewareDeleteError, MessageMiddlewareDisconnectedError, MessageMiddlewareMessageError
 
 class MessageMiddlewareQueueRabbitMQ(MessageMiddlewareQueue):
 
     def __init__(self, host, queue_name):
-        pass
+        self.connection =  pika.BlockingConnection(pika.ConnectionParameters(host))
+        self.channel = self.connection.channel()
+        self.channel.queue_declare(queue=queue_name, durable=True, arguments={'x-queue-type': 'quorum'})
+        self.queue_name = queue_name
+
+    def send(self, message):
+        self.channel.basic_publish(exchange='',
+                                routing_key=self.queue_name,
+                                body=message,
+                                properties=pika.BasicProperties(
+                                    delivery_mode = pika.DeliveryMode.Persistent
+                                ))
+        
+    def close(self):
+       self.connection.close()
+
+    def stop_consuming(self):
+        self.channel.stop_consuming()
+
+    def start_consuming(self, on_message_callback):
+        def callback(ch, method, properties, body):
+           def ack():
+               ch.basic_ack(delivery_tag=method.delivery_tag)
+           def nack():
+               ch.basic_nack(delivery_tag=method.delivery_tag)
+           on_message_callback(body, ack, nack)
+
+        self.channel.basic_qos(prefetch_count=1)
+        self.channel.basic_consume(queue=self.queue_name,
+                                   on_message_callback=callback)
+        self.channel.start_consuming()
+
+
 
 class MessageMiddlewareExchangeRabbitMQ(MessageMiddlewareExchange):
     
