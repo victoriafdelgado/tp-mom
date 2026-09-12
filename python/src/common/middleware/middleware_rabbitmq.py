34a5b17cc857ff8452c36f29ec822fa4aba3fe57
victoriafdelgado <vickyfdelgado@gmail.com>
2026-09-12 18:20:01 -0300

    implemento exchange (faltan excepciones)

--- python/src/common/middleware/middleware_rabbitmq.py
+++ python/src/common/middleware/middleware_rabbitmq.py
@@ -18,10 +18,9 @@ class MessageMiddlewareQueueRabbitMQ(MessageMiddlewareQueue):
                                 properties=pika.BasicProperties(
                                     delivery_mode = pika.DeliveryMode.Persistent
                                 ))
-        
     def close(self):
-       self.connection.close()
-
+        self.connection.close()
+    
     def stop_consuming(self):
         self.channel.stop_consuming()
 
@@ -39,8 +38,41 @@ class MessageMiddlewareQueueRabbitMQ(MessageMiddlewareQueue):
         self.channel.start_consuming()
 
 
-
 class MessageMiddlewareExchangeRabbitMQ(MessageMiddlewareExchange):
     
     def __init__(self, host, exchange_name, routing_keys):
-        pass
+        self.connection =  pika.BlockingConnection(pika.ConnectionParameters(host))
+        self.channel = self.connection.channel()
+        self.channel.exchange_declare(exchange=exchange_name, exchange_type='direct')
+        self.exchange_name = exchange_name
+        self.result = self.channel.queue_declare(queue='', exclusive=True)
+        self.queue_name = self.result.method.queue
+        self.routing_keys = routing_keys
+        for routing_key in self.routing_keys:
+            self.channel.queue_bind(exchange=self.exchange_name, 
+                                    queue=self.queue_name,
+                                    routing_key=routing_key)
+    
+    def send(self, message):
+        for routing_key in self.routing_keys:
+            self.channel.basic_publish(exchange=self.exchange_name, 
+                                    routing_key=routing_key, 
+                                    body=message)
+
+    def close(self):
+        self.connection.close()
+
+    def stop_consuming(self):
+        self.channel.stop_consuming()
+
+    def start_consuming(self, on_message_callback):
+        def callback(ch, method, properties, body):
+                def ack():
+                    ch.basic_ack(delivery_tag=method.delivery_tag)
+                def nack():
+                    ch.basic_nack(delivery_tag=method.delivery_tag)
+                on_message_callback(body, ack, nack)
+
+        self.channel.basic_consume(queue=self.queue_name, on_message_callback=callback)
+        self.channel.start_consuming()
+    
